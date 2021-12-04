6cb13d1a3d9c57d45998d8882572463c4a5c24c5
Lea Pehar <leapehar@gmail.com>
2021-12-04 02:19:47 +0000

    removed unnecessary comments

--- typewriter.js
+++ typewriter.js
@@ -2,7 +2,6 @@ const sentence = "hello there from lighthouse labs";
 
 let delay = 0;
 for (const char of sentence) {
-  //process.stdout.write(char);
   setTimeout(() => {
     process.stdout.write(char);
   }, delay)
